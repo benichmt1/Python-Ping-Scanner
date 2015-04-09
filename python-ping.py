5918462025963d198755d12e3daae1390ada628f
benichmt1 <benichmt1@gmail.com>
2015-04-09 15:48:03 -0400

    Create python-ping.py

---
+++ python-ping.py
@@ -0,0 +1,26 @@
+#A single-threaded Python Ping Scanner
+#To do - work on the multithreading capabilities. Right now the output is being parsed which prevents the other thread from firing?
+
+#!/usr/bin/python
+import subprocess
+target = 200
+up = 0
+down = 0
+while (target < 255):
+        ip = "192.168.31." +str(target)
+        output = subprocess.Popen(["ping","-c","1",ip],stdout = subprocess.PIPE).communicate()[0]
+
+        if ('Unreachable' in output):
+                print 'Host ' + ip + " is offline or unavailable"
+                down+= 1
+        else:
+                print "Host " + ip + " is online"
+                up+= 1
+
+        target = target+1
+
+
+print "A total of " + str(up+down) + " hosts were scanned."
+print str(up) + " hosts were alive, and " + str(down) + " hosts were unreachable. "
+quit()
+
